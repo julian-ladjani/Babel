dd7509912976cceeaf4bb1f6bcde8245cddc2ed2
yannick <yannick.suc@epitech.eu>
2018-09-18 17:28:25 +0200

    [Qt] Add first implementation of window

---
+++ src/client/windows/Window.cpp
@@ -0,0 +1,33 @@
+//
+// EPITECH PROJECT, 2018
+// CPP_babel_2018
+// File description:
+// Window.cpp
+//
+
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QPushButton>
+#include "QApplication"
+#include "Window.hpp"
+
+babel::client::MainWindow::MainWindow()
+{
+	resize(1280, 720);
+	show();
+	setWindowTitle(QApplication::translate(
+		"Epyks", "Epyks, Grand et impuissant !"));
+	setStyleSheet("background-color:#2B2B2B;");
+	QLabel  *label  = new QLabel;
+	QPixmap pixmap_img("src/assets/logo.png");
+	pixmap_img = pixmap_img.scaled(
+		400, pixmap_img.height() * 400 / pixmap_img.width(),
+		Qt::IgnoreAspectRatio);
+	label->setPixmap(pixmap_img);
+	label->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+	QHBoxLayout *layout = new QHBoxLayout;
+	layout->addWidget(label);
+
+	setLayout(layout);
+	show();
+}
---
+++ src/client/windows/Window.hpp
@@ -0,0 +1,26 @@
+//
+// EPITECH PROJECT, 2018
+// CPP_babel_2018
+// File description:
+// Window.hpp
+//
+
+#ifndef CPP_BABEL_2018_WINDOW_HPP
+#define CPP_BABEL_2018_WINDOW_HPP
+
+#include <QtWidgets/QMainWindow>
+
+namespace babel {
+	namespace client {
+		class MainWindow : public QWidget
+		{
+		public:
+			MainWindow();
+
+		private:
+		};
+
+	}
+}
+
+#endif /* CPP_BABEL_2018_WINDOW_HPP */
