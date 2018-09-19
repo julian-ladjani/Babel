ab2afa841d0ea7e6ec186bb8d6b8599c2f8d7420
gregory.eple <gregory.eple@localhost.localdomain>
2018-09-19 14:35:57 +0200

    [Common] added User and ConnectionInfos

---
+++ src/common/User.cpp
@@ -0,0 +1,42 @@
+//
+// EPITECH PROJECT, 2018
+// CPP_babel_2018 - User.cpp
+// File description:
+// Created by Gregory EPLE
+//
+
+#include "User.hpp"
+
+babel::common::User::User(std::string login, int id, bool connected) :
+	_login(login), _id(id), _connected(connected)
+{}
+
+const std::string &babel::common::User::getLogin() const
+{
+	return _login;
+}
+
+void babel::common::User::setLogin(const std::string &login)
+{
+	_login = login;
+}
+
+int babel::common::User::getId() const
+{
+	return _id;
+}
+
+void babel::common::User::setId(int id)
+{
+	_id = id;
+}
+
+bool babel::common::User::isConnected() const
+{
+	return _connected;
+}
+
+void babel::common::User::setConnected(bool connected)
+{
+	_connected = connected;
+}
