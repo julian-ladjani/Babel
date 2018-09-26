45b5efee0e85dbf552eef068ec60a6ac5d0f5746
gregory.eple <gregory.eple@localhost.localdomain>
2018-09-26 14:18:03 +0200

    [AudioManager] restart stream when over/under flow

--- src/client/sound/AudioManager.cpp
+++ src/client/sound/AudioManager.cpp
@@ -19,16 +19,13 @@ babel::client::AudioManager::AudioManager() :
 babel::client::AudioManager::~AudioManager()
 {
 	PaError paErr;
-	if (_recording)
-		stopRecording();
-	if (_streaming)
-		stopStream();
+	closeStream();
 	paErr = Pa_Terminate();
 	if (paErr != paNoError)
 		throwPortAudioError(paErr);
 }
 
-std::vector<unsigned short> babel::client::AudioManager::getRecord() const
+std::vector<unsigned short> babel::client::AudioManager::getRecord()
 {
 	PaError paErr;
 	std::vector<unsigned short> record(_bufferSize);
@@ -39,19 +36,26 @@ std::vector<unsigned short> babel::client::AudioManager::getRecord() const
 	else
 		paErr = Pa_ReadStream(_stream, record.data(), _bufferSize);
 	if (paErr != paNoError)
-		throwPortAudioError(paErr);
+		restartStream();
 	return record;
 }
 
 void babel::client::AudioManager::playRecord(
-	std::vector<unsigned short> record) const
+	std::vector<unsigned short> record)
 {
 	PaError paErr;
 	while (Pa_GetStreamWriteAvailable(_stream) < record.size());
 	paErr = Pa_WriteStream(_stream, record.data(),
 			       record.size());
 	if (paErr != paNoError)
-		throwPortAudioError(paErr);
+		restartStream();
+}
+
+void babel::client::AudioManager::restartStream()
+{
+	closeStream();
+	startStream();
+	startRecording();
 }
 
 void babel::client::AudioManager::startStream()
@@ -86,6 +90,14 @@ void babel::client::AudioManager::stopRecording()
 	if (paErr != paNoError)
 		throwPortAudioError(paErr);
 	_recording = false;
+}
+
+void babel::client::AudioManager::closeStream()
+{
+	if (_recording)
+		stopRecording();
+	if (_streaming)
+		stopStream();
 }
 
 uint32_t babel::client::AudioManager::getChannel() const
